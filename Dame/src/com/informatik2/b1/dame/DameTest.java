fed5dbaae036da25aefe8709836f26544cb24860
Baris.Erol <Baris.Erol@student.reutlingen-university.de>
2015-10-07 22:11:22 +0200

    new Package

---
+++ Dame/src/com/informatik2/b1/dame/DameTest.java
@@ -0,0 +1,14 @@
+package com.informatik2.b1.dame;
+
+public class DameTest {
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+		System.out.println("Zweiter Test");
+		System.out.println("Test");
+System.out.println("Test");
+System.out.println("Test III");
+System.out.println("Test IV");
+	}
+
+}
