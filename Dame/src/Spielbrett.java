1e6a25db64ae42c63d297613897ce9709b146056
DanielAyen <daniel.ayen@student.reutlingen-university.de>
2015-10-07 12:36:16 +0200

    Brett Test

--- Dame/src/Spielbrett.java
+++ Dame/src/Spielbrett.java
@@ -1,14 +1,20 @@
-
 public class Spielbrett {
 
 	/**
 	 * 
 	 */
-	
+
 	private String[][] Brett = new String[12][12];
-	
-	
-	
-	
-	
+
+	public void sysoBrett() {
+
+		for (int i = 0; i < Brett.length; i++) {
+			System.out.println(Brett[i]);
+
+			for (int j = 0; j < Brett[i].length; j++) {
+				System.out.println(Brett[i][j]);
+			}
+		}
+
+	}
 }
