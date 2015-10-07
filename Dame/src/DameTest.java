9ee4414d8ae612ce8f20e99bd5872677fb0c276f
DanielAyen <daniel.ayen@student.reutlingen-university.de>
2015-10-07 11:34:34 +0200

    Dritter Test

--- Dame/src/DameTest.java
+++ Dame/src/DameTest.java
@@ -4,6 +4,7 @@ public class DameTest {
 	public static void main(String[] args) {
 		System.out.println("Test");
 		System.out.println("Zweiter Test");
+		System.out.println("Dritter Test");
 
 	}
 
