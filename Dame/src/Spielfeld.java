07c92c4ef13006b66d41adb7897f2edb9d082033
SimonEiberger <Simon.Eiberger@reutlingen-university.de>
2015-10-08 13:41:50 +0200

    Klasse Spielfeld angefangen

--- Dame/src/Spielfeld.java
+++ Dame/src/Spielfeld.java
@@ -1,7 +1,50 @@
 
 public class Spielfeld {
 	
+	private Spielbrett Brett;
+	private Spielfigur spielfigur;
+	private String id;
+	private boolean istBelegt;
+	private FarbEnum farbe;
+	
+	public Spielfeld(String id){
+		Spielfeld[][] brett = new Spielfeld[12][12];
+		for ( int i = 0; i<=11; i++){
+			
+			for (int j = 0; j<=11; j++){
+				
+			}
+		}
+		this.setid(id);
+	}
+	
+	public Spielfeld ( String id, Spieler spieler){
+		
+	}
 
-	private Spieler spieler;
+	
+	public String getid(){
+		return id;
+		
+	}
+	private void setid(String id) {
+		this.id = id;
+		
+	}
+	public Spielfigur getSpielfigur(){
+		return this.spielfigur;
+	}
+	
+	private void setSpielfigur(){
+		this.spielfigur = spielfigur;
+	}
+
+	public boolean isIstBelegt() {
+		return istBelegt;
+	}
+
+	public void setIstBelegt(boolean istBelegt) {
+		this.istBelegt = istBelegt;
+	}
 
 }
