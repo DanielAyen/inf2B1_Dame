bf8ceab29149a04256e7739b0f9bd0cb979e0c37
SimonEiberger <Simon.Eiberger@reutlingen-university.de>
2015-10-09 15:38:42 +0200

    Konstruktor im Spielfeld editiert

--- Dame/src/Spielfeld.java
+++ Dame/src/Spielfeld.java
@@ -2,30 +2,18 @@
 public class Spielfeld {
 	
 	
-	private Spielbrett Brett;
+	private Spielbrett spielbrett;
 	private Spielfigur spielfigur;
 	private String id;
 	private boolean istBelegt;
 	private FarbEnum farbe;
 	
-	public Spielfeld(String id){
-		Spielfeld[][] brett = new Spielfeld[12][12];
-		for ( int i = 1; i<=12; i++){
-			for (int j = 1; j<=12; j++){
-		brett[i][j].setid("A" + i);
-		if (i >= 12 && j>= 12 ){
-			brett[i][j].setid("B" + i);
+	public Spielfeld(Spielbrett spielbrett){
+		this.spielbrett = spielbrett;
+			
 		}
-		if (i >= 12 && j>= 12 ){
-			brett[i][j].setid("C" + i);	
-			}
-		if (i >= 12 && j>= 12 ){
-			brett[i][j].setid("D" + i);
-		}
-			}
-	}}
 	
-	public Spielfeld ( String id, Spieler spieler){
+	public Spielfeld ( String id, Spielfigur spielfigur){
 		
 	}
 
