fd296af6300aef28853e89cc71efe85e453500b2
DanielAyen <Daniel.Ayen@student.reutlingen-university.de>
2015-10-13 15:54:25 +0200

    Stein gefixt

--- Dame/src/Stein.java
+++ Dame/src/Stein.java
@@ -3,40 +3,17 @@
  * @author Baris,Daniel,Simon
  *
  */
-public class Stein extends Spielfigur{
+public class Stein extends Spielfigur {
 
 	/**
 	 * 
 	 * @param id
 	 * @param farbe
 	 */
-	
-	private int x= 1;
+
 	public Stein(int id, FarbEnum farbe) {
 		super(id, farbe);
-<<<<<<< HEAD
-		System.out.println("Servus");
-		System.out.println("!egee");
-		System.out.println("Test");
-		System.out.println("egbehge");
-=======
-		x = x;
-		x= x;
-		x=x;
-		x=x;#ihigogzugzuuiugigzgzigzij9#
-		j9
-		uj9ju9uj9j+ß#
-		9+hj
-		9hj+
-		
-		9hj
-		uj9+9hjß+ßj9+9jß#
->>>>>>> branch 'master' of https://github.com/DanielAyen/inf2B1_Dame
 
 	}
-	
-
-	
-	
 
 }
