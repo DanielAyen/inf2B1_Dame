70e8a0261409a083462c605182ebc07f990c7020
SimonEiberger <Simon.Eiberger@student.reutlingen-university.de>
2015-10-13 14:40:44 +0200

    Attribute, Konstruktor und Getter/Setter zu Spielfigur hinzugefügt

--- Dame/src/Spielfigur.java
+++ Dame/src/Spielfigur.java
@@ -4,8 +4,71 @@
  *
  */
 public abstract class Spielfigur {
+	/**
+	 * Attribute
+	 * 
+	 * @param farbe
+	 * @param ID
+	 * @param pos
+	 */
 
-	public Spielfigur() {
+	private FarbEnum farbe;
+	private int position; // Wird später benötigt
+	private int id;
+	private int counter = 0; // Wird später benötigt
+	private Spieler spieler;
 
+	/**
+	 * Konstruktor
+	 * 
+	 * @param id
+	 * @param farbe
+	 * 
+	 *          Dem Konstruktor werden eine ID und die Farbe der Spielfigur
+	 *          übergeben
+	 */
+
+	public Spielfigur(int id, FarbEnum farbe) {
+		this.setFarbe(farbe);
+	}
+
+	/**
+	 * Getter für Farbe
+	 * 
+	 * @return farbe
+	 */
+
+	public FarbEnum getFarbe() {
+		return farbe;
+	}
+
+	/**
+	 * Setter für die Farbe
+	 * 
+	 * @param farbe
+	 */
+
+	public void setFarbe(FarbEnum farbe) {
+		this.farbe = farbe;
+	}
+
+	/**
+	 * Getter für ID
+	 * 
+	 * @return id
+	 */
+
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Setter für die id
+	 * 
+	 * @param id
+	 */
+
+	public void setId(int id) {
+		this.id = id;
 	}
 }
