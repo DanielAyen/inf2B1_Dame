94a953eb9a7942c75d214bea50f83d8852403d6a
SimonEiberger <Simon.Eiberger@student.reutlingen-university.de>
2015-10-19 15:17:32 +0200

    JUnit Tests für Spielbrett und Spielfeld

--- Dame/src/SpielbrettTest.java
+++ Dame/src/SpielbrettTest.java
@@ -1,13 +1,48 @@
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 
 public class SpielbrettTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private static Spielbrett sb;
+	private static Spielbrett sb2; 
+	
+	@BeforeClass
+	public static void Methode(){
+		sb = new Spielbrett(12); 
 	}
+	
+	
+	@Test
+	public void NullTest() {
+		assertNotNull(sb);
+	}
+	
+	@Test 
+	public void FeldNichtNull(){
+		assertNotNull(sb.getEinzelFeld(0, 0));
+		assertNotNull(sb.getEinzelFeld(0, 11));
+		assertNotNull(sb.getEinzelFeld(11, 0));
+		assertNotNull(sb.getEinzelFeld(11, 11));
+		
+		
+	}
+	
+	@Test
+	public void Test(){
+		assertNotSame(sb.getEinzelFeld(3, 9), sb.getEinzelFeld(6, 6));
+	}
+	
+	@Test // Sollte nicht machbar sein
+	public void BrettAnzahl(){
+		sb2 = new Spielbrett(8);
+	}
+	
+	@Test
+	public void  FeldNull(){
+		 assertNull(sb.getEinzelFeld(15, 13));
+	 }
 
 }
--- Dame/src/SpielfeldTest.java
+++ Dame/src/SpielfeldTest.java
@@ -1,13 +1,69 @@
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 
 public class SpielfeldTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+
+		private static Spielfeld sf;
+		private static Spielbrett sb;
+		
+		@BeforeClass
+		public static void Methode(){
+			
+			sb = new Spielbrett(12);
+			sf = new Spielfeld(sb, true, 0, 9);
+		}
+		@Test 
+		public void Klassentest(){
+			assertNotNull(sf.getClass());
+		}
+		
+		
+		@Test
+		public void posXnichtNulltest() {
+		assertNotNull(sf.getPosX());
+		}
+		
+		@Test
+		public void posXtestkorrekt(){
+			assertTrue(sf.getPosX() == 0);
+		}
+		
+		@Test
+		public void posXtestfalsch(){
+			assertFalse(sf.getPosX() == 6);
+		}
+		
+		
+		@Test
+		public void posYnichtNulltest() {
+		assertNotNull(sf.getPosY());
+		}
+		
+		@Test
+		public void posYtestkorrekt(){
+			assertTrue(sf.getPosY() == 9);
+		}
+		
+		@Test
+		public void posYtestfalsch(){
+			assertFalse(sf.getPosY() == 14);
+		}
+		
+		@Test
+		public void feldbelegttest(){
+			sf.setIstBelegt(true);
+			assertTrue(sf.getIstBelegt());
+		}
+		
+		@Test
+		public void feldfreitest(){
+			sf.setIstBelegt(false);
+			assertFalse(sf.getIstBelegt());
+		}
+
 
 }
