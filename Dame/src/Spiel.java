4e0e1d24472f88ab4991270a142dc8efee3888f1
DanielAyen <Daniel.Ayen@student.reutlingen-university.de>
2015-10-23 12:22:56 +0200

    Fig erstellen angepasst switch angepasst erweitert

--- Dame/src/Spiel.java
+++ Dame/src/Spiel.java
@@ -11,10 +11,9 @@ public class Spiel implements iBediener {
 
 	private boolean spielAufgebaut = false;
 	private int spielerAnzahl = 0;
-	private boolean erstellenBeendet = false;
 	private String name;
-	private String farbe = "ungesetzt";
-	private String ki = "ungesetzt";
+	private String farbes;
+	private String ki;
 	private boolean spiellaeuft = false;
 	private boolean schwarzvergeben = false;
 	private boolean weissvergeben = false;
@@ -25,7 +24,7 @@ public class Spiel implements iBediener {
 	private Spielfeld spielfeld;
 
 	public void spielStarten() {
-
+		System.out.println("Bitte erstelle zuerst ein neues Spielbrett. Falls du Hilfe benoetigst gebe 'help' in die konsole ein");
 		try {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			String eingabe = "";
@@ -34,11 +33,15 @@ public class Spiel implements iBediener {
 				eingabe = reader.readLine().toLowerCase();
 				switch (eingabe) {
 				case "help":
-					System.out.println("aufbauen : Erstellt ein Spielbrett, wird zum spielen benoetigt.\n");
-					System.out.println("spieler erstellen : Erlaubt dir einen Spieler zu erstellen, es werden 2 Spieler zum spielen benoetigt.\n");
-					System.out.println("start: Startet das Spiel, es wird ein erstelltes Spielbrett und zwei Spieler benoetigt.\n");
-					System.out.println("beenden : Das Spiel wird geschlossen.\n");
-					System.out.println("ziehen : Erlaubt dir eine Spielfigur zu bewegen. Nicht moeglich solange das Spiel nicht laeuft.\n");
+					System.out.println("aufbauen : Erstellt ein Spielbrett, wird zum spielen benoetigt.");
+					System.out.println("spieler erstellen : Erlaubt dir einen Spieler zu erstellen, es werden 2 Spieler zum spielen benoetigt.");
+					System.out.println("start: Startet das Spiel, es wird ein erstelltes Spielbrett und zwei Spieler benoetigt.");
+					System.out.println("beenden : Das Spiel wird geschlossen.");
+					System.out.println("ziehen : Erlaubt dir eine Spielfigur zu bewegen. Nicht moeglich solange das Spiel nicht laeuft.");
+					System.out.println("anzeigenb : Zeigt dir das Spielbrett.");
+					System.out.println("anzeigenf : Zeigt dir alle Spielfiguren.");
+					System.out.println("speichern : erlaubt es dir das Spiel zu speichern.");
+					System.out.println("laden : erlaubt es dir ein Spielstand zu laden.");
 					break;
 				// zum erstellen des spielfelds
 				case "aufbauen":
@@ -57,10 +60,32 @@ public class Spiel implements iBediener {
 						System.out.println("Fehlerhafte Eingabe, bitte nur 8 , 10 oder 12 eingeben. Zurueck im Hauptmenue.\n");
 						break;
 					}
-
+					// zum anzeigen des Bretts
+				case "anzeigenb":
+					if (spielAufgebaut) {
+						brett.display();
+						System.out.println("\n");
+						break;
+					} else {
+						System.out.println("Du kannst kein Brett anzeigen das nicht existiert! Nutze aufbauen um ein Brett zu erstellen.");
+						break;
+					}
+					// Zum anzeigen der Figuren
+				case "anzeigenf":
+					if (weissvergeben == true && schwarzvergeben == true) {
+						brett.display();
+						System.out.println("\n");
+						break;
+					} else {
+						System.out.println("Du kannst keine Figuren anzeigen die nicht existieren! Nutze spieler erstellen um einen Spieler und seine Figuren zu erstellen.");
+						break;
+					}
 					// zum erstellen von spielern
 				case "spieler erstellen":
-
+					if (!spielAufgebaut) {
+						System.out.println("Du musst zuerst ein Spielbrett aufbauen! (aufbauen)");
+						break;
+					}
 					System.out.println("Spielernamen eingeben (min 2 Zeichen!)\n");
 					name = reader.readLine();
 					if (name == null || name.length() < 2) {
@@ -132,40 +157,6 @@ public class Spiel implements iBediener {
 						break;
 					}
 
-					/*
-					 * System.out.println("Spielernamen eingeben\n"); name =
-					 * reader.readLine();
-					 * System.out.println("spielerfarbe eingeben (s für schwarz w für weiß)"
-					 * );
-					 * 
-					 * do { farbe = reader.readLine(); // farben switch switch (farbe) {
-					 * 
-					 * // case 1 farbe case "s":
-					 * 
-					 * System.out.println(
-					 * "ist der Spieler eine Ki oder ein Mensch? (m für Mensch k für Ki)"
-					 * ); do { ki = reader.readLine(); // innerrer ki switch switch (ki) {
-					 * // case 1 ki case "m": Spieler s1 = new Spieler(name,
-					 * FarbEnum.SCHWARZ, false); break; // case 2 ki case "k": Spieler s2
-					 * = new Spieler(name, FarbEnum.SCHWARZ, true); break; // default für
-					 * ki default: System.out.println("weder k noch m eingeben."); } }
-					 * while (!ki.equals("m") || (!ki.equals("k")));
-					 * 
-					 * break; // case 2 farbe case "w": System.out.println(
-					 * "ist der Spieler eine Ki oder ein Mensch? (m für Mensch k für Ki)"
-					 * ); ki = reader.readLine(); // innerer ki switch do { switch (ki) {
-					 * // case 1 ki case "m": Spieler s1 = new Spieler(name,
-					 * FarbEnum.SCHWARZ, false); break; // case 2 ki case "k": Spieler s2
-					 * = new Spieler(name, FarbEnum.SCHWARZ, true); break; // default für
-					 * ki default: System.out.println("weder k noch m eingeben."); } }
-					 * while (!ki.equals("m") || (!ki.equals("k"))); break;
-					 * 
-					 * // default für farbe default:
-					 * System.out.println("weder s noch w eingeben.");
-					 * 
-					 * } } while (!farbe.equals("w") || (!farbe.equals("s")));
-					 */
-
 					// lässt das spiel beginnen
 				case "start":
 					if (spielAufgebaut == true && spielerAnzahl == 2) {
@@ -189,15 +180,22 @@ public class Spiel implements iBediener {
 				case "beenden":
 					System.out.println("\n\n\t\tDas Spiel wird beendet.");
 					break;
+				// speichern
+				case "speichern":
+					System.out.println("Derzeit nicht implementiert -Sorry.");
+					break;
+				// laden
+				case "laden":
+					System.out.println("Derzeit nicht implementiert -Sorry.");
+					break;
 				// falls falsche eingaben erfolgen
 				default:
-					System.err.println("Befehl nicht erkannt");
+					System.err.println("Befehl nicht erkannt.");
 
 				}
-
+				// solange man spielt läuft es weiter.
 			} while (!eingabe.equals("beenden"));
 
-			// Keeps going unless you enter "finish game"
 		} catch (Exception e) {
 
 			e.printStackTrace();
@@ -281,7 +279,7 @@ public class Spiel implements iBediener {
 
 		if (spieler.getFarbe() == FarbEnum.SCHWARZ) {
 			for (int i = 0; i < brett.getBrettGroesse() / 2 - 1; i++) {
-				for (int j = 0; j < brett.getBrettGroesse() / 2 - 1; j++) {
+				for (int j = 0; j < brett.getBrettGroesse(); j++) {
 					if (brett.getBrettFeld(i, j).getIstBelegt() == false && brett.getBrettFeld(i, j).getIstSchwarz() == true) {
 						spieler.addSpielfigur(new Spielfigur(FarbEnum.SCHWARZ, i, j, brett, false));
 					}
@@ -289,7 +287,7 @@ public class Spiel implements iBediener {
 			}
 		} else {
 			for (int i = brett.getBrettGroesse(); i > brett.getBrettGroesse() / 2 - 1; i--) {
-				for (int j = brett.getBrettGroesse(); j > brett.getBrettGroesse() / 2 - 1; j--) {
+				for (int j = brett.getBrettGroesse(); j > brett.getBrettGroesse(); j--) {
 					if (brett.getBrettFeld(i, j).getIstBelegt() == false && brett.getBrettFeld(i, j).getIstSchwarz() == true) {
 						spieler.addSpielfigur(new Spielfigur(FarbEnum.WEIß, i, j, brett, false));
 					}
@@ -384,4 +382,37 @@ public class Spiel implements iBediener {
 	public void laden() {
 
 	}
+
+	/*
+	 * System.out.println("Spielernamen eingeben\n"); name = reader.readLine();
+	 * System.out.println("spielerfarbe eingeben (s für schwarz w für weiß)" );
+	 * 
+	 * do { farbe = reader.readLine(); // farben switch switch (farbe) {
+	 * 
+	 * // case 1 farbe case "s":
+	 * 
+	 * System.out.println(
+	 * "ist der Spieler eine Ki oder ein Mensch? (m für Mensch k für Ki)" ); do {
+	 * ki = reader.readLine(); // innerrer ki switch switch (ki) { // case 1 ki
+	 * case "m": Spieler s1 = new Spieler(name, FarbEnum.SCHWARZ, false); break;
+	 * // case 2 ki case "k": Spieler s2 = new Spieler(name, FarbEnum.SCHWARZ,
+	 * true); break; // default für ki default:
+	 * System.out.println("weder k noch m eingeben."); } } while (!ki.equals("m")
+	 * || (!ki.equals("k")));
+	 * 
+	 * break; // case 2 farbe case "w": System.out.println(
+	 * "ist der Spieler eine Ki oder ein Mensch? (m für Mensch k für Ki)" ); ki =
+	 * reader.readLine(); // innerer ki switch do { switch (ki) { // case 1 ki
+	 * case "m": Spieler s1 = new Spieler(name, FarbEnum.SCHWARZ, false); break;
+	 * // case 2 ki case "k": Spieler s2 = new Spieler(name, FarbEnum.SCHWARZ,
+	 * true); break; // default für ki default:
+	 * System.out.println("weder k noch m eingeben."); } } while (!ki.equals("m")
+	 * || (!ki.equals("k"))); break;
+	 * 
+	 * // default für farbe default:
+	 * System.out.println("weder s noch w eingeben.");
+	 * 
+	 * } } while (!farbe.equals("w") || (!farbe.equals("s")));
+	 */
+
 }
