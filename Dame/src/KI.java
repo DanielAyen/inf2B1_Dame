9c4442d8ed258d5eaee0e8cf1a31d79937e392f3
HannesRaschke <Hannes.Raschke@student.reutlingen-university.de>
2015-11-01 23:56:10 +0100

    KI umstrukturiert

--- Dame/src/KI.java
+++ Dame/src/KI.java
@@ -2,15 +2,13 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-
 /**
  * 
  * @author Baris, Daniel, Simon, Hannes
  *
  */
-public abstract class KI  implements Serializable{
-	
-	
+public abstract class KI implements Serializable {
+
 	/**
 	 * 
 	 */
@@ -18,84 +16,93 @@ public abstract class KI  implements Serializable{
 	private Spieler spieler;
 	private Spielbrett brett;
 
-
 	/**
 	 * Konstruktor der KI
 	 * 
 	 * @param spieler
 	 */
-	
+
 	public KI(Spieler spieler) {
-		if(spieler==null){
+		if (spieler == null) {
 			throw new RuntimeException("Ki darf nicht ohne Spieler exisitieren");
-		}else{
-		this.spieler=spieler;
+		} else {
+			this.spieler = spieler;
 		}
 	}
-	
-	public Spieler getSpieler(){
-		return this.spieler;}
-	
-	
-	private KI zug() {
 
-		//Methode erfasst eigene Spielfiguren, um später mit getZuege mögliche Züge zu ermitteln.
-		
+	public Spieler getSpieler() {
+		return this.spieler;
+	}
+
+	private int zugRichtung() {
+		if (spieler.getFarbe() == FarbEnum.WEIß) {
+			return -1;
+		} else {
+			return +1;
+		}
+	}
+
+	private void zug() {
+
+		// Methode erfasst eigene Spielfiguren, um später mit getZuege mögliche Züge
+		// zu ermitteln.
+
+		// ArrayList<ArrayList<int[]>> alleZuge = new ArrayList<ArrayList<int[]>>();
 		for (int i = 0; i < brett.getBrettGroesse(); i++) {
 			for (int j = 0; j < brett.getBrettGroesse(); j++) {
 				Spielfeld feld = brett.getBrettFeldIndex(i, j);
-				if (feld.getIstSchwarz()) {
-					if (feld.getIstBelegt()) {
-						Spielfigur figur = feld.getSpielfigur();
-						if (figur.getFarbe() == spieler.getFarbe()) {
-							// this.getZuege(i,j,figur);
+				if (feld.getIstSchwarz() && feld.getIstBelegt()) {
+					Spielfigur figur = feld.getSpielfigur();
+					if (figur.getFarbe() == spieler.getFarbe()) {
+						ArrayList<int[]> zuege = this.getZuege(i, j, figur);
+						if (!zuege.isEmpty()) {
+							zuege.get(0);
+							// TODO movemethode aufrufen
+							return;
 						}
 					}
 				}
 			}
 		}
-		return null;
 	}
-	
-	
-	//fragt für Spielfigur ab welche züge möglich sind
-	//TODO bewertung der Züge
-	
-	private ArrayList<int[]>  getZuege(int i, int j, Spielfigur figur) {
-		ArrayList<int[]> zuge = new ArrayList<int[]> ();
-		if (figur.getDame(figur)) {
+
+	// fragt für Spielfigur ab welche züge möglich sind
+	// TODO bewertung der Züge
+
+	private ArrayList<int[]> getZuege(int i, int j, Spielfigur figur) {
+		ArrayList<int[]> zuge = new ArrayList<int[]>();
+		if (!figur.getDame(figur)) {
 			if (i < brett.getBrettGroesse() - 1 && j < brett.getBrettGroesse()) {
-				Spielfeld zugFeld1 = brett.getBrettFeldIndex(i + 1, j + 1);
-				if(zugFeld1.getIstBelegt()){
-					//auf farbe prüfen, überspringen möglich?
-				}else{
-					int [] koord = {i+1,j+1};
+
+				Spielfeld zugFeld1 = brett.getBrettFeldIndex(i + 1, j + this.zugRichtung());
+				if (zugFeld1.getIstBelegt()) {
+					// auf farbe prüfen, überspringen möglich?
+				} else {
+					int[] koord = { i + 1, j + this.zugRichtung() };
 					zuge.add(koord);
-					
+
 				}
 			}
 			if (i > 0 && j < brett.getBrettGroesse()) {
-				Spielfeld zugFeld2 = brett.getBrettFeldIndex(i - 1, j + 1);
-				if(zugFeld2.getIstBelegt()){
-					//auf farbe prüfen, überspringen möglich?
-				}else{
-					int [] koord = {i-1,j+1};
+				Spielfeld zugFeld2 = brett.getBrettFeldIndex(i - 1, j + this.zugRichtung());
+				if (zugFeld2.getIstBelegt()) {
+					// auf farbe prüfen, überspringen möglich?
+				} else {
+					int[] koord = { i - 1, j + this.zugRichtung() };
 					zuge.add(koord);
-					
 				}
 			}
 		} else {
-				//abfrage für dame
+			// abfrage für dame
 		}
 		return zuge;
-		
-		
+
 	}
 
-//	private KI zug(){
-//		Iterator<Spielfigur> alleFiguren = spieler.getAlleFiguren().iterator();
-//	while(alleFiguren.hasNext()){
-//	}
-//	return null;
-//	}
+	// private KI zug(){
+	// Iterator<Spielfigur> alleFiguren = spieler.getAlleFiguren().iterator();
+	// while(alleFiguren.hasNext()){
+	// }
+	// return null;
+	// }
 }
