6d5d5f66bc3661bddfa46901803d5bfde1964c39
HannesRaschke <Hannes.Raschke@student.reutlingen-university.de>
2015-11-05 15:12:34 +0100

    KI kann schlagen

--- Dame/src/KI.java
+++ Dame/src/KI.java
@@ -1,6 +1,6 @@
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Iterator;
+//import java.util.Iterator;
 
 /**
  * 
@@ -20,6 +20,7 @@ public abstract class KI implements Serializable {
 	 * Konstruktor der KI
 	 * 
 	 * @param spieler
+	 * @param brett
 	 */
 
 	public KI(Spieler spieler, Spielbrett brett) {
@@ -35,6 +36,9 @@ public abstract class KI implements Serializable {
 		return this.spieler;
 	}
 
+	/**
+	 * Ermittelt die Zugrichtung.
+	 */
 	private int zugRichtung() {
 		if (spieler.getFarbe() == FarbEnum.WEIß) {
 			return -1;
@@ -43,9 +47,19 @@ public abstract class KI implements Serializable {
 		}
 	}
 
+	private int zugRichtung2() {
+		if (spieler.getFarbe() == FarbEnum.WEIß) {
+			return -2;
+		} else {
+			return +2;
+		}
+	}
+
+	/**
+	 * Erfasst eigene Spielfiguren, um später mit getZuege mögliche Züge zu
+	 * ermitteln.
+	 */
 	public int[] zug() {
-		// Methode erfasst eigene Spielfiguren, um später mit getZuege mögliche Züge
-		// zu ermitteln.
 
 		// ArrayList<ArrayList<int[]>> alleZuge = new ArrayList<ArrayList<int[]>>();
 		for (int i = 0; i < brett.getBrettGroesse(); i++) {
@@ -55,11 +69,20 @@ public abstract class KI implements Serializable {
 				if (feld.getIstSchwarz() && feld.getIstBelegt()) {
 					Spielfigur figur = feld.getSpielfigur();
 					if (figur.getFarbe() == spieler.getFarbe()) {
-						ArrayList<int[]> zuege = this.getZuege(a, j, figur);
-						if (!zuege.isEmpty()) {
-							int[] zugZielKoords = zuege.get(0);
-							int rückgabe[] = { a, j, zugZielKoords[0], zugZielKoords[1] };
-							return rückgabe;
+						ArrayList<int[]> schlaege = this.getSchlaege(a, j, figur);
+						if (!schlaege.isEmpty()) {
+							System.out.println("hol die schläge");
+							int[] zugZielKoords1 = schlaege.get(0);
+							int rueckgabeSchlaege[] = { a, j, zugZielKoords1[0], zugZielKoords1[1] };
+							return rueckgabeSchlaege;
+						} else {
+							System.out.println("hol die züge");
+							ArrayList<int[]> zuege = this.getZuege(a, j, figur);
+							if (!zuege.isEmpty()) {
+								int[] zugZielKoords2 = zuege.get(0);
+								int rueckgabeBewegung[] = { a, j, zugZielKoords2[0], zugZielKoords2[1] };
+								return rueckgabeBewegung;
+							}
 						}
 					}
 				}
@@ -71,6 +94,61 @@ public abstract class KI implements Serializable {
 	// fragt für Spielfigur ab welche züge möglich sind
 	// TODO bewertung der Züge
 
+	private ArrayList<int[]> getSchlaege(char a, int j, Spielfigur figur) {
+		ArrayList<int[]> schlaege = new ArrayList<int[]>();
+
+		if (!figur.getDame(figur)) {
+			System.out.println("keine dame");
+			// wäre ein schritt nach vorne noch im spielbrett?
+			if (j + this.zugRichtung2() >= 1 && j + this.zugRichtung2() <= brett.getBrettGroesse()) {
+				System.out.println("schritt möglich");
+				// ist schräger schritt nach rechts möglich?
+				if (a + 2 <= 'a' + brett.getBrettGroesse() - 1) {
+					System.out.println("brettgrößengrenzen passen");
+					Spielfeld zugFeld1 = brett.getBrettFeldSchachnotation((char) (a + 1), j + this.zugRichtung());
+					if (zugFeld1.getIstBelegt() == true) {
+						System.out.println("feld belegt");
+						Spielfigur figurAufBelegtemFeld = zugFeld1.getSpielfigur();
+
+						// schaut ob Figur andere Farbe hat
+						if (figurAufBelegtemFeld.getFarbe() != spieler.getFarbe()) {
+System.out.println("gegner andere farbe");
+							// überprüft ob Feld hinter GegnerFigur frei ist
+							Spielfeld zugFeld2 = brett.getBrettFeldSchachnotation((char) (a + 2), j + this.zugRichtung2());
+							if (zugFeld2.getIstBelegt() == false) {
+								int[] koordSchlagen = { (a + 2), (j + (this.zugRichtung2())) };
+								schlaege.add(koordSchlagen);
+							}
+						}
+					}
+					System.out.println("stein gleiche farbe");
+				}
+				// ist schräger schritt nach links möglich?
+				if (a - 2 >= 'a') {
+					Spielfeld zugFeld1 = brett.getBrettFeldSchachnotation((char) (a - 1), j + this.zugRichtung());
+					if (zugFeld1.getIstBelegt() == true) {
+						Spielfigur figurAufBelegtemFeld = zugFeld1.getSpielfigur();
+						// schaut ob Figur andere Farbe hat
+						if (figurAufBelegtemFeld.getFarbe() != spieler.getFarbe()) {
+							// überprüft ob Feld hinter GegnerFigur frei ist
+							Spielfeld zugFeld2 = brett.getBrettFeldSchachnotation((char) (a - 2), j + this.zugRichtung2());
+							if (zugFeld2.getIstBelegt() == false) {
+								int[] koordSchlagen = { (a - 2), (j + (this.zugRichtung2())) };
+								schlaege.add(koordSchlagen);
+							}
+						}
+					}
+				}
+			}
+		}
+		return schlaege;
+	}
+
+	
+	
+	/**
+	 * Findet für übergebene Figur die möglichen Züge
+	 */
 	private ArrayList<int[]> getZuege(char a, int j, Spielfigur figur) {
 		ArrayList<int[]> zuge = new ArrayList<int[]>();
 
@@ -80,20 +158,15 @@ public abstract class KI implements Serializable {
 				// ist schräger schritt nach rechts möglich?
 				if (a + 1 <= 'a' + brett.getBrettGroesse() - 1) {
 					Spielfeld zugFeld1 = brett.getBrettFeldSchachnotation((char) (a + 1), j + this.zugRichtung());
-					if (zugFeld1.getIstBelegt()) {
-						// auf farbe prüfen, überspringen möglich?
-					} else {
+					if (zugFeld1.getIstBelegt() == false) {
 						int[] koord = { a + 1, j + this.zugRichtung() };
 						zuge.add(koord);
-
 					}
 				}
 				// ist schräger schritt nach links möglich?
 				if (a - 1 >= 'a') {
 					Spielfeld zugFeld2 = brett.getBrettFeldSchachnotation((char) (a - 1), j + this.zugRichtung());
-					if (zugFeld2.getIstBelegt()) {
-						// auf farbe prüfen, überspringen möglich?
-					} else {
+					if (zugFeld2.getIstBelegt() == false) {
 						int[] koord = { a - 1, j + this.zugRichtung() };
 						zuge.add(koord);
 					}
