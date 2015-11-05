1bd5f3630cd3914fde8c720c85c06af0e5b0310d
HannesRaschke <Hannes.Raschke@student.reutlingen-university.de>
2015-11-05 15:24:05 +0100

    KI gibt ihre Züge auf Konsole an

--- Dame/src/KI.java
+++ Dame/src/KI.java
@@ -71,16 +71,16 @@ public abstract class KI implements Serializable {
 					if (figur.getFarbe() == spieler.getFarbe()) {
 						ArrayList<int[]> schlaege = this.getSchlaege(a, j, figur);
 						if (!schlaege.isEmpty()) {
-							System.out.println("hol die schläge");
 							int[] zugZielKoords1 = schlaege.get(0);
 							int rueckgabeSchlaege[] = { a, j, zugZielKoords1[0], zugZielKoords1[1] };
+							System.out.println("Figur zog von " + a+j+" nach " + (char)zugZielKoords1[0] + zugZielKoords1[1]+".");
 							return rueckgabeSchlaege;
 						} else {
-							System.out.println("hol die züge");
 							ArrayList<int[]> zuege = this.getZuege(a, j, figur);
 							if (!zuege.isEmpty()) {
 								int[] zugZielKoords2 = zuege.get(0);
 								int rueckgabeBewegung[] = { a, j, zugZielKoords2[0], zugZielKoords2[1] };
+								System.out.println("Figur zog von " + a+j+" nach " + (char)zugZielKoords2[0] + zugZielKoords2[1]+".");
 								return rueckgabeBewegung;
 							}
 						}
@@ -98,30 +98,25 @@ public abstract class KI implements Serializable {
 		ArrayList<int[]> schlaege = new ArrayList<int[]>();
 
 		if (!figur.getDame(figur)) {
-			System.out.println("keine dame");
 			// wäre ein schritt nach vorne noch im spielbrett?
 			if (j + this.zugRichtung2() >= 1 && j + this.zugRichtung2() <= brett.getBrettGroesse()) {
-				System.out.println("schritt möglich");
 				// ist schräger schritt nach rechts möglich?
 				if (a + 2 <= 'a' + brett.getBrettGroesse() - 1) {
-					System.out.println("brettgrößengrenzen passen");
 					Spielfeld zugFeld1 = brett.getBrettFeldSchachnotation((char) (a + 1), j + this.zugRichtung());
 					if (zugFeld1.getIstBelegt() == true) {
-						System.out.println("feld belegt");
 						Spielfigur figurAufBelegtemFeld = zugFeld1.getSpielfigur();
 
 						// schaut ob Figur andere Farbe hat
 						if (figurAufBelegtemFeld.getFarbe() != spieler.getFarbe()) {
-System.out.println("gegner andere farbe");
 							// überprüft ob Feld hinter GegnerFigur frei ist
 							Spielfeld zugFeld2 = brett.getBrettFeldSchachnotation((char) (a + 2), j + this.zugRichtung2());
 							if (zugFeld2.getIstBelegt() == false) {
 								int[] koordSchlagen = { (a + 2), (j + (this.zugRichtung2())) };
 								schlaege.add(koordSchlagen);
+								System.out.println("Hat Figur an Stelle " + koordSchlagen +" geschlagen.");
 							}
 						}
 					}
-					System.out.println("stein gleiche farbe");
 				}
 				// ist schräger schritt nach links möglich?
 				if (a - 2 >= 'a') {
@@ -135,6 +130,7 @@ System.out.println("gegner andere farbe");
 							if (zugFeld2.getIstBelegt() == false) {
 								int[] koordSchlagen = { (a - 2), (j + (this.zugRichtung2())) };
 								schlaege.add(koordSchlagen);
+								System.out.println("Hat Figur an Stelle " + koordSchlagen +" geschlagen.");
 							}
 						}
 					}
